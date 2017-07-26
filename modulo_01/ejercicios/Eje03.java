1847f00c2416a14f66f569f74e8059580a2382af
claudioaltamiranda <claudioaltamiranda@yahoo.com.ar>
2017-07-26 12:28:51 -0300

    Actualizando

--- modulo_01/ejercicios/Eje03.java
+++ modulo_01/ejercicios/Eje03.java
@@ -1,5 +1,16 @@
+/*
+	Enunciado Escribir el algoritmo que, a partir 
+	de la cantidad de bancos de un aula y la 
+	cantidad de alumnos inscriptos para un curso, 
+	permita determinar si alcanzan los bancos 
+	existentes. De no ser así, informar además 
+	cuantos bancos sería necesario agregar. El 
+	usuario deberá ingresar por teclado tanto 
+	la cantidad de bancos que tiene el aula, 
+	como la cantidad de alumnos inscriptos 
+	para el curso.
+*/
 import java.util.Scanner;
-
 
 class Eje03{
 	public static void main(String[] args) {
@@ -9,19 +20,23 @@ class Eje03{
 	 	int alumnInscrp;
 	 	int bancFaltantes;
 
-	 	System.out.print ("Ingrese cantidad de bancos disponibles en el aula: ");
+	 	System.out.print("Ingrese cantidad de bancos disponibles en el aula: ");
 	 	cantidadBancAula = sc.nextInt();
-	 	System.out.print ("Ingrese cantidad de alumnos inscriptos: ");
+	 	System.out.print("Ingrese cantidad de alumnos inscriptos: ");
 	 	alumnInscrp = sc.nextInt();
 	 	
 	 	if (cantidadBancAula >= alumnInscrp){
 
-	 		System.out.print("Los bancos del aula son suficientes. " );
+	 		System.out.println("Los bancos del aula son suficientes." );
 
 	 	}else{
+
 	 		bancFaltantes = alumnInscrp - cantidadBancAula;
 	 	
-	 		System.out.println("La cantidad de bancos faltantes es: " +  bancFaltantes);
+	 		System.out.println("La cantidad de bancos faltantes es: " +  
+	 			bancFaltantes);
 	 	}
+
  	}
+
 }
