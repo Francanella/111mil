d87048d01f911f4f07c3fd9dd4387fc9e13c53e7
claudioaltamiranda <claudioaltamiranda@yahoo.com.ar>
2017-08-01 06:15:27 -0300

    Agregado java01.txt y programacion01.txt

---
+++ modulo_01/java/Ejemplo.java
@@ -0,0 +1,11 @@
+class Ejemplo{
+	public static void main(String[] args){
+		int a = 2, b = 1;
+
+		System.out.println("Ordenar dos números diferentes");
+		if(a < b)
+			System.out.println(a + " - " + b);
+		else
+			System.out.println(b + " - " + a);
+	}
+}
---
+++ modulo_01/java/Hola.java
@@ -0,0 +1,12 @@
+/*
+	Primer programa en Java
+	Autor: Claudio
+*/
+
+class Hola{
+
+	public static void main(String[] args){
+		System.out.println("Hola mundo!");
+	}
+
+}
