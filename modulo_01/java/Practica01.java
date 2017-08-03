d82ec571a97adc6f60b10a85b56450777aa71733
claudioaltamiranda <claudioaltamiranda@yahoo.com.ar>
2017-08-03 19:22:24 -0300

    Subidos ejercicios de practica

--- modulo_01/java/Practica01.java
+++ modulo_01/java/Practica01.java
@@ -2,33 +2,37 @@
 	Operadores relacionales
 */
 
-import java.util.Scanner;
+import java.util.Scanner;		
 
 class Practica01{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);	// declaración e inicialización
 			/*
 				System.in es un elemento de la clase System que
-					representa la entrada estándar
+					representa la entrada estándar - teclado
 				System.out es un elemento de la clase System que
-					representa la salida estándar
+					representa la salida estándar - pantalla
 			*/
 
 		System.out.print("Como te llamas: ");
 		String nombre = sc.next();	// next() permite leer un texto
 																// desde el teclado
 
+		String apellido = "Pérez";
+
+		System.out.println("Hola, " + nombre + " " + apellido);
+
 		System.out.print("Ingrese un número: ");
-		int a = sc.nextInt();			// nextInt() permite leer un entero 
-															// desde el teclado
+		double a = sc.nextDouble();			// nextDouble() permite leer un decimal 
+																		// desde el teclado
 		System.out.print("Ingrese otro número: ");
 		int b = sc.nextInt();
 
-		System.out.println("a == b: " + (a == b));
-		System.out.println("a != b: " + (a != b));
-		System.out.println("a < b:  " + (a < b));
-		System.out.println("a <= b: " + (a <= b));
-		System.out.println("a > b:  " + (a > b));
-		System.out.println("a >= b: " + (a >= b));
+		System.out.println("a == b: " + (a == b));	// == significa comparación de igualdad
+		System.out.println("a != b: " + (a != b));	// != distinto
+		System.out.println("a < b:  " + (a < b));		// < menor que
+		System.out.println("a <= b: " + (a <= b));	// <= menor o igual que 
+		System.out.println("a > b:  " + (a > b));		// > mayor que
+		System.out.println("a >= b: " + (a >= b));	// >= mayor o igual que
 	}
 }
---
+++ modulo_01/java/Prueba.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+class Prueba{
+	public static void main(String[] args){
+		System.out.print("Ho\tla");
+	}
+}
