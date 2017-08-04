612fc3e9ad142976628c9071fe53631926dc8535
claudioaltamiranda <claudioaltamiranda@yahoo.com.ar>
2017-08-03 21:22:37 -0300

    Agregado hasta el Practica03.java

---
+++ modulo_01/java/Practica03.java
@@ -0,0 +1,34 @@
+/*
+	Otro ejemplo en Java
+*/
+class Practica03 {
+	public static void main(String args[]) {
+		int a = 2147423432;
+		byte b = -128;
+		short s = 31000;
+		long l = 21474234322345L;			// El sufijo L permite manejar
+																	// literales enteros-largos
+
+		float f = 3.1416F;						// El sufijo F permite manejar
+																	// literales de tipo float
+		double d = 3.0000000000000000000001;
+
+		char c = 'F' + 32;
+
+		boolean bb = 10 > 100;
+
+		System.out.println("Entero: " + a);
+		System.out.println("Byte: " + b);
+		System.out.println("Short: " + s);
+		System.out.println("Long: " + l);
+
+
+		System.out.println("Float: " + f);
+		System.out.println("Double: " + d);
+
+
+		System.out.println('8' - '0');
+
+		System.out.println(bb);
+	}
+}
