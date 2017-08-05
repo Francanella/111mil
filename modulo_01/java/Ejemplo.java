4a2229b0a6abe6c9c136a97d4758ae6108ded02c
claudioaltamiranda <claudioaltamiranda@yahoo.com.ar>
2017-08-04 21:31:56 -0300

    Actualizado 04 08 2017

--- modulo_01/java/Ejemplo.java
+++ modulo_01/java/Ejemplo.java
@@ -1,27 +1,23 @@
-/* 
-	Ejemplo de if .. else
-	Práctica:
-	Indicar cual es el mayor de dos números
-	leídos desde el teclado
+/*
+	Ejemplo de if..else anidados
+	el valor medio de tres números distintos
+	leídos por teclado
 */
-
-import java.util.Scanner;
-
 class Ejemplo{
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int a;
-		int b;
-
-		System.out.println("Ingrese dos números:");
-		System.out.print("a: ");
-		a = sc.nextInt();
-		System.out.print("b: ");
-		b = sc.nextInt();
+		int a = 10;
+		int b = 60; 
+		int c = 80;
 
 		if(a > b)
-			System.out.println("Mayor es a");
+			if(a > c)
+				System.out.println("Mayor: " + a);
+			else
+				System.out.println("Mayor: " + c);
 		else
-			System.out.println("b es mayor o igual");
+			if(b > c)
+				System.out.println("Mayor: " + b);
+			else
+				System.out.println("Mayor: " + c);
 	}
 }
