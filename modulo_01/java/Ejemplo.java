9efdf4ff45d762e591482a418040df10e81df7e3
claudioaltamiranda <claudioaltamiranda@yahoo.com.ar>
2017-08-06 11:13:25 -0300

    Actualizado 06 08 2017

--- modulo_01/java/Ejemplo.java
+++ modulo_01/java/Ejemplo.java
@@ -1,23 +1,8 @@
-/*
-	Ejemplo de if..else anidados
-	el valor medio de tres números distintos
-	leídos por teclado
-*/
 class Ejemplo{
-	public static void main(String[] args){
-		int a = 10;
-		int b = 60; 
-		int c = 80;
-
-		if(a > b)
-			if(a > c)
-				System.out.println("Mayor: " + a);
-			else
-				System.out.println("Mayor: " + c);
-		else
-			if(b > c)
-				System.out.println("Mayor: " + b);
-			else
-				System.out.println("Mayor: " + c);
+	public static void main(String args[]) {
+		int a = 1;
+		{ // crea un nuevo ámbito
+		int a = 2; // Error! a está definida previamente
+		}
 	}
 }
--- modulo_01/java/Prueba.java
+++ modulo_01/java/Prueba.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 
 class Prueba{
+
 	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+		// Scanner sc = new Scanner(System.in);
 
-		System.out.print("Ingrese algo: ");
-
-		String algo = sc.next();
-
-		System.out.println( algo.length() );
+		System.out.println("Esta \" es una comilla");
+		
 	}
+
 }
