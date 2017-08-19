7d0a8d66422c7a4c4820ce9ced9248777860d30f
claudioaltamiranda <claudioaltamiranda@yahoo.com.ar>
2017-08-19 16:51:31 -0300

    Actualizado 19 agosto 2017

--- modulo_01/java/Ejemplo.java
+++ modulo_01/java/Ejemplo.java
@@ -1,34 +1,21 @@
 /*
-
+	operador if ternario
+	if <exprl>?a:b
+	if(<exprl>)
+		a
+	else
+		b
 */
-
-import java.util.Scanner;
-
-class Ejemplo{
-	public static void main(String[] args){
-		boolean[] colectivo = new boolean[10];
-		Scanner sc = new Scanner(System.in);
-		for(int i=0; i<colectivo.length; i++)
-			colectivo[i] = false;
-
-		int asiento = 0;
-		while(asiento != -1){
-			System.out.print("Asiento: ");
-			asiento = sc.nextInt();
-			if(asiento != -1){
-				if(colectivo[asiento-1])
-					System.out.println("Ya está ocupado!!!");
-				colectivo[asiento-1] = true;
-			}
-		}
-
-		for(int i=0; i<colectivo.length; i++){
-			System.out.print("Asiento[" + i + "]: ");
-			if(colectivo[i])
-				System.out.println("Ocupado");
-			else
-				System.out.println("Libre");
-		}
-		System.out.println();
+class Ejemplo {
+	public static void main(String args[]) {
+		int i, k;
+		i = 10;
+		k = i < 0 ? -i : i; // valor absoluto de i
+		System.out.print("Valor absoluto de ");
+		System.out.println(i + " es " + k);
+		i = -10;
+		k = i < 0 ? -i : i; // valor absoluto i
+		System.out.print("Valor absoluto de ");
+		System.out.println(i + " es " + k);
 	}
 }
