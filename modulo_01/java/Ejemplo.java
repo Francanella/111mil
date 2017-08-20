663da8ff39098498a52a10f5a653e5a0881ec375
claudioaltamiranda <claudioaltamiranda@yahoo.com.ar>
2017-08-20 17:47:19 -0300

    Actualizado al 20 ago 2017

--- modulo_01/java/Ejemplo.java
+++ modulo_01/java/Ejemplo.java
@@ -1,21 +1,9 @@
-/*
-	operador if ternario
-	if <exprl>?a:b
-	if(<exprl>)
-		a
-	else
-		b
-*/
 class Ejemplo {
 	public static void main(String args[]) {
-		int i, k;
-		i = 10;
-		k = i < 0 ? -i : i; // valor absoluto de i
-		System.out.print("Valor absoluto de ");
-		System.out.println(i + " es " + k);
-		i = -10;
-		k = i < 0 ? -i : i; // valor absoluto i
-		System.out.print("Valor absoluto de ");
-		System.out.println(i + " es " + k);
+		boolean t = true;
+		System.out.println("Antes del return.");
+		if(t) 
+			return; // Retorna a quien llamó el método
+		System.out.println("ESTO NO SE EJECUTARA.");
 	}
 }
