d1ddd7770f8435ff521141ac4b9edaacd6080c58
claudioaltamiranda <claudioaltamiranda@yahoo.com.ar>
2017-09-01 17:33:30 -0300

    Agregado repaso 01 sep 2017

---
+++ modulo_01/java/Secuencial.java
@@ -0,0 +1,31 @@
+/*
+    Búsqueda secuencial
+*/
+
+class Secuencial{
+	public static void main(String[] args){
+        // Inicializar el array
+		int[] numeros = {6,5,3,1,8,7,2,4};
+
+        // Mostrar el array antes de buscar
+        for(int i=0; i<numeros.length; i++)
+        	System.out.print("[" + numeros[i] + "] ");
+        System.out.println();
+        
+        boolean encontrado = false;
+        int i, buscado = 17;
+
+        for(i=0; i<numeros.length;i++){       
+            if(buscado == numeros[i]){
+                encontrado = true;
+                break;
+            }
+        }
+
+        if(encontrado){
+            System.out.println("Encontrado en la posicion: " + i);
+        }else{
+            System.out.println("Elemento no encontrado!!!");
+        }
+	}
+}
