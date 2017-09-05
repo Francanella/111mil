d106d7f1425543186f567242e17d916704021998
claudioaltamiranda <claudioaltamiranda@yahoo.com.ar>
2017-09-05 05:43:00 -0300

    Actualizado todos los ejercicios y ejemplos de estruturas repetitivas en ejercicios.txt y ejemplos.txt

--- modulo_01/programacion/Ejemplo.java
+++ modulo_01/programacion/Ejemplo.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 class Ejemplo {
 	public static void main(String args[]) {
-		boolean t = true;
-		System.out.println("Antes del return.");
-		if(t) 
-			return; // Retorna a quien llamó el método
-		System.out.println("ESTO NO SE EJECUTARA.");
+		Scanner sc = new Scanner(System.in);
+		double a, b, c, media;
+		String nombre;
+
+		do{
+			System.out.print("Ingrese el nombre: ");
+			nombre = sc.next();
+			if(!nombre.equals("***")){
+				System.out.print("Nota en Basic: ");
+				a = sc.nextDouble();
+				System.out.print("Nota en Pascal: ");
+				b = sc.nextDouble();
+				System.out.print("Nota en FORTRAN: ");
+				c = sc.nextDouble();
+				media = (a + b + c) / 3;
+				System.out.println("Alumno:   " + nombre);
+				System.out.println("Promedio: " + media);
+			}
+		}while(!nombre.equals("***"));
+
+		System.out.println("FIN");
 	}
 }
