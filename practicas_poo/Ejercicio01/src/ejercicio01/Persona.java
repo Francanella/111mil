0a84ee60d5194de221b2d48d01b9e72f3bcce3c6
claudioaltamiranda <claudioaltamiranda@yahoo.com.ar>
2018-08-25 10:56:28 -0300

    Add files via upload

--- practicas_poo/Ejercicio01/src/ejercicio01/Persona.java
+++ practicas_poo/Ejercicio01/src/ejercicio01/Persona.java
@@ -5,4 +5,36 @@ public class Persona {
     private String nombre;
     private String apellido;
     private char sexo;    
+    
+    public Persona(){
+        this.edad = 0;
+        this.nombre = "";
+        this.apellido = "";
+        this.sexo = 'x';
+    }
+    
+    public int getEdad(){
+        return this.edad;
+    }
+    public void setEdad(int e){
+        this.edad = e;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String n){
+        this.nombre = n;
+    }
+    public String getApellido(){
+        return this.apellido;
+    }
+    public void setApellido(String a){
+        this.apellido = a;
+    }
+    public char getSexo(){
+        return this.sexo;
+    }
+    public void setSexo(char s){
+        this.sexo = s;
+    }
 }
