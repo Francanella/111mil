9e1b4715c44f5debb20cb77d801cd751e3a54e59
Claudio Altamiranda <claudioaltamiranda@yahoo.com.ar>
2018-08-27 19:51:37 -0300

    UML.png

---
+++ practicas_poo/Ejercicio02/src/ejercicio02/Autor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio02;
+
+/**
+ *
+ * @author claudioaltamiranda
+ */
+public class Autor {
+    private String nombre;
+    private String nacionalidad;
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String n){
+        this.nombre = n;
+    }
+    public String getNacionalidad(){
+        return this.nacionalidad;
+    }
+    public void setNacionalidad(String n){
+        this.nacionalidad = n;
+    }
+}
