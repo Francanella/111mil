7ed0ae38a3e77ecbe4af013d19834a26781428cd
Claudio Altamiranda <claudioaltamiranda@yahoo.com.ar>
2018-08-29 19:14:45 -0300

    Modificado Personaje.java

--- practicas_poo/Ejercicio03/src/ejercicio03/Personaje.java
+++ practicas_poo/Ejercicio03/src/ejercicio03/Personaje.java
@@ -14,5 +14,22 @@ public class Personaje {
     private int fuerza;
     private int salud;
     
-    
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String n){
+        this.nombre = n;
+    }
+    public int getFuerza(){
+        return this.fuerza;
+    }
+    public void setFuerza(int f){
+        this.fuerza = f;
+    }
+    public int getSalud(){
+        return this.salud;
+    }
+    public void setSalud(int s){
+        this.salud = s;
+    }
 }
