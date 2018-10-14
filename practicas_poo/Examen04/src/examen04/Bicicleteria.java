c9a579623046f45415b9874b8849a603c666c1fb
Claudio Altamiranda <claudioaltamiranda@yahoo.com.ar>
2018-10-14 15:01:42 -0300

    Examen 04

--- practicas_poo/Examen04/src/examen04/Bicicleteria.java
+++ practicas_poo/Examen04/src/examen04/Bicicleteria.java
@@ -5,10 +5,28 @@
  */
 package examen04;
 
+import java.util.List;
+import java.util.ArrayList;
 /**
  *
  * @author claudioaltamiranda
  */
 public class Bicicleteria {
+    private List<Bicicleta> bicicletas;
+    private float ganancias;
+    private Integer cantidadDeVentas;
     
+    public Bicicleteria(){
+        this.bicicletas = new ArrayList<Bicicleta>();
+        this.ganancias = 0.0F;
+        this.cantidadDeVentas = 0;
+    }
+    public void addBicicleta(Bicicleta nuevaBici){
+        bicicletas.add(nuevaBici);
+    }
+    public void venderBicicleta(Bicicleta bicicleta){
+        if(this.bicicletas.remove(bicicleta)){
+            this.cantidadDeVentas++;
+        }
+    }
 }
